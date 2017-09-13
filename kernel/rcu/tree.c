e5c53adc44a3e38e20804b62790c26bc1cb756b3
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2017-09-13 14:43:41 +0200

    srcu: use cpu_online() instead custom check
    The current check via srcu_online is slightly racy because after looking
    at srcu_online there could be an interrupt that interrupted us long
    enough until the CPU we checked against went offline.
    An alternative would be to hold the hotplug rwsem (so the CPUs don't
    change their state) and then check based on cpu_online() if we queue it
    on a specific CPU or not. queue_work_on() itself can handle if something
    is enqueued on an offline CPU but a timer which is enqueued on an offline
    CPU won't fire until the CPU is back online.
    
    I am not sure if the removal in rcu_init() is okay or not. I assume that
    SRCU won't enqueue a work item before SRCU is up and ready.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -3793,8 +3793,6 @@ int rcutree_online_cpu(unsigned int cpu)
 {
 	sync_sched_exp_online_cleanup(cpu);
 	rcutree_affinity_setting(cpu, -1);
-	if (IS_ENABLED(CONFIG_TREE_SRCU))
-		srcu_online_cpu(cpu);
 	return 0;
 }
 
@@ -3805,8 +3803,6 @@ int rcutree_online_cpu(unsigned int cpu)
 int rcutree_offline_cpu(unsigned int cpu)
 {
 	rcutree_affinity_setting(cpu, cpu);
-	if (IS_ENABLED(CONFIG_TREE_SRCU))
-		srcu_offline_cpu(cpu);
 	return 0;
 }
 
@@ -4256,8 +4252,6 @@ void __init rcu_init(void)
 	for_each_online_cpu(cpu) {
 		rcutree_prepare_cpu(cpu);
 		rcu_cpu_starting(cpu);
-		if (IS_ENABLED(CONFIG_TREE_SRCU))
-			srcu_online_cpu(cpu);
 	}
 
 	/* Create workqueue for expedited GPs and for Tree SRCU. */

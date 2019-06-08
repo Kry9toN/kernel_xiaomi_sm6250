f2d9a150970746876bd8139a4fc49fab223a9504
MarijnS95 <marijns95@gmail.com>
2019-06-08 12:20:27 +0200

    platform: msm: ipav3: Correct smallest ipa3_write string to 1.
    The smallest string that this function deals with is "1", which is of
    size 1. Correct the if to allow this case.
    
    Signed-off-by: MarijnS95 <marijns95@gmail.com>
    Signed-off-by: Ahmad Thoriq Najahi <najahi@chips-projects.xyz>

--- drivers/platform/msm/ipa/ipa_v3/ipa.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa.c
@@ -6140,8 +6140,8 @@ static ssize_t ipa3_write(struct file *file, const char __user *buf,
 	int ret = 0;
 	int i = 0;
 
-	if (count < 2)
-		return -EFAULT;
+	if (count < 1)
+		return -EINVAL;
 
 	dbg_buff = kmalloc((count + 1) * sizeof(char), GFP_KERNEL);
 	if (!dbg_buff)
@@ -6206,7 +6206,7 @@ static ssize_t ipa3_write(struct file *file, const char __user *buf,
 		} else if (strcmp(dbg_buff, "1")) {
 			IPAERR("got invalid string %s not loading FW\n",
 				dbg_buff);
-			goto end_msg;
+			goto end;
 		}
 		pr_info("IPA is loading with %sMHI configuration\n",
 			ipa3_ctx->ipa_config_is_mhi ? "" : "non ");

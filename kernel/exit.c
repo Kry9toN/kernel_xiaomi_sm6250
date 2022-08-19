166ed8731ecc681960dfc9c5115596169bdf1c9a
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2022-08-19 11:24:44 +0200

    rcu: Introduce rcuwait.
    This is an all in one commit backporting rcuwait:
    - update.c, rcuwait.h as of commit
       58d4292bd037b ("rcu: Uninline multi-use function: finish_rcuwait()")
    - exit.c as of commit
       9d9a6ebfea329 ("rcuwait: Let rcuwait_wake_up() return whether or not a task was awoken")
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Message-Id: <20220819092446.980320-8-bigeasy@linutronix.de>
    Signed-off-by: Luis Claudio R. Goncalves <lgoncalv@redhat.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -279,8 +279,9 @@ repeat:
 		goto repeat;
 }
 
-void rcuwait_wake_up(struct rcuwait *w)
+int rcuwait_wake_up(struct rcuwait *w)
 {
+	int ret = 0;
 	struct task_struct *task;
 
 	rcu_read_lock();
@@ -288,21 +289,24 @@ void rcuwait_wake_up(struct rcuwait *w)
 	/*
 	 * Order condition vs @task, such that everything prior to the load
 	 * of @task is visible. This is the condition as to why the user called
-	 * rcuwait_trywake() in the first place. Pairs with set_current_state()
+	 * rcuwait_wake() in the first place. Pairs with set_current_state()
 	 * barrier (A) in rcuwait_wait_event().
 	 *
 	 *    WAIT                WAKE
-	 *    [S] tsk = current	  [S] cond = true
-	 *        MB (A)	      MB (B)
-	 *    [L] cond		  [L] tsk
+	 *    [S] tsk = current   [S] cond = true
+	 *        MB (A)              MB (B)
+	 *    [L] cond            [L] tsk
 	 */
 	smp_mb(); /* (B) */
 
 	task = rcu_dereference(w->task);
 	if (task)
-		wake_up_process(task);
+		ret = wake_up_process(task);
 	rcu_read_unlock();
+
+	return ret;
 }
+EXPORT_SYMBOL_GPL(rcuwait_wake_up);
 
 /*
  * Determine if a process group is "orphaned", according to the POSIX

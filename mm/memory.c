f6586b06df237b175fad6ccee63bf129219ba373
Yu Zhao <yuzhao@google.com>
2022-12-30 14:52:51 -0700

    mm: add vma_has_recency()
    This patch adds vma_has_recency() to indicate whether a VMA may
    exhibit temporal locality that the LRU algorithm relies on.
    
    This function returns false for VMAs marked by VM_SEQ_READ or
    VM_RAND_READ. While the former flag indicates linear access, i.e., a
    special case of spatial locality, both flags indicate a lack of
    temporal locality, i.e., the reuse of an area within a relatively
    small duration.
    
    "Recency" is chosen over "locality" to avoid confusion between
    temporal and spatial localities.
    
    Before this patch, the active/inactive LRU only ignored the accessed
    bit from VMAs marked by VM_SEQ_READ. After this patch, the
    active/inactive LRU and MGLRU share the same logic: they both ignore
    the accessed bit if vma_has_recency() returns false.
    
    For the active/inactive LRU, the following fio test showed a [6, 8]%
    increase in IOPS when randomly accessing mapped files under memory
    pressure.
    
      kb=$(awk '/MemTotal/ { print $2 }' /proc/meminfo)
      kb=$((kb - 8*1024*1024))
    
      modprobe brd rd_nr=1 rd_size=$kb
      dd if=/dev/zero of=/dev/ram0 bs=1M
    
      mkfs.ext4 /dev/ram0
      mount /dev/ram0 /mnt/
      swapoff -a
    
      fio --name=test --directory=/mnt/ --ioengine=mmap --numjobs=8 \
          --size=8G --rw=randrw --time_based --runtime=10m \
          --group_reporting
    
    The discussion that led to this patch is here [1]. Additional test
    results are available in that thread.
    
    [1] https://lore.kernel.org/r/Y31s%2FK8T85jh05wH@google.com/
    
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- mm/memory.c
+++ mm/memory.c
@@ -1365,8 +1365,7 @@ again:
 					force_flush = 1;
 					set_page_dirty(page);
 				}
-				if (pte_young(ptent) &&
-				    likely(!(vma->vm_flags & VM_SEQ_READ)))
+				if (pte_young(ptent) && likely(vma_has_recency(vma)))
 					mark_page_accessed(page);
 			}
 			rss[mm_counter(page)]--;
@@ -4262,8 +4261,8 @@ static int __handle_mm_fault(struct vm_area_struct *vma, unsigned long address,
 #ifdef CONFIG_LRU_GEN
 static void lru_gen_enter_fault(struct vm_area_struct *vma)
 {
-	/* the LRU algorithm doesn't apply to sequential or random reads */
-	current->in_lru_fault = !(vma->vm_flags & (VM_SEQ_READ | VM_RAND_READ));
+	/* the LRU algorithm only applies to accesses with recency */
+	current->in_lru_fault = vma_has_recency(vma);
 }
 
 static void lru_gen_exit_fault(void)
